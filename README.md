098bc1fee2691414ed75b8f9aacb85f177b76ac4
yurapa <yura.paray@gmail.com>
2017-10-29 10:49:17 +0200

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# start_html
+# Start HTML/CSS/JS and Gulp/Bower template with Bootstrap 4
---
+++ src/assets/js/main.js
@@ -0,0 +1,7 @@
+//= ../../../bower_components/jquery/dist/jquery.js
+//= ../../../bower_components/popper.js/dist/umd/popper.js
+//= ../../../bower_components/bootstrap/js/dist/util.js
+//= ../../../bower_components/bootstrap/js/dist/collapse.js
+
+
+//= custom/carousel.js
